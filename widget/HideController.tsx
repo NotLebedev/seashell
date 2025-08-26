afe7ef5f2bcf000361b1c08727b90f7c7bf535f1
NotLebedev <NotLebedev>
2025-08-26 21:09:40 +0300

    Change AutoHide to unhide on empty desktop

--- widget/HideController.tsx
+++ widget/HideController.tsx
@@ -1,30 +1,45 @@
 import { Gtk } from "ags/gtk4";
 import { ParentProps } from "./util";
-import { createState } from "ags";
+import { createComputed, createState } from "ags";
+import AstalHyprland from "gi://AstalHyprland?version=0.1";
+
+function isEmptyWorkspace(state: AstalHyprland.Hyprland): boolean {
+  return state.focused_workspace.clients.length === 0;
+}
 
 /**
  * Component ot auto hide its contents and show on hover
  */
 export default function AutoHide({ children }: ParentProps) {
-  const [hidden, setHidden] = createState(false);
+  const [hovered, setHovered] = createState(true);
+  const [emptyWorkspace, setEmptyWorkspace] = createState(
+    isEmptyWorkspace(AstalHyprland.get_default()),
+  );
+
+  const displayed = createComputed(
+    [hovered, emptyWorkspace],
+    (hovered, emptyWorkspace) => hovered || emptyWorkspace,
+  );
 
   const enterController = new Gtk.EventControllerMotion();
   const leaveController = new Gtk.EventControllerMotion();
 
-  enterController.connect("enter", () => setHidden(false));
-  leaveController.connect("leave", () => setHidden(true));
+  AstalHyprland.get_default().connect("notify::focused-workspace", (state) => {
+    setEmptyWorkspace(isEmptyWorkspace(state));
+  });
+
+  enterController.connect("enter", () => setHovered(true));
+  leaveController.connect("leave", () => setHovered(false));
 
   return (
     <overlay class="hideController">
-      <box hexpand class={hidden((h) => (h ? "hidden hider" : "hider"))}>
+      <box
+        hexpand
+        class={displayed((d) => (d ? "hider" : "hidden hider"))}
+        $={(self) => self.add_controller(leaveController)}
+      >
         {children}
       </box>
-      <box // Big overlay box that tracks leave after hover
-        $type="overlay"
-        halign={Gtk.Align.FILL}
-        valign={Gtk.Align.FILL}
-        $={(self) => self.add_controller(leaveController)}
-      />
       <box // Smaller overlay box that tracks entry
         $type="overlay"
         height_request={10}
