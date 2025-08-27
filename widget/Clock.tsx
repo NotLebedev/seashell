33d64e72e2fbf0cde4ada9549da786fb24722ada
NotLebedev <NotLebedev>
2025-08-27 16:29:11 +0300

    Add BarComponent and Clock
    Add base for all bar components and simple Clock component

---
+++ widget/Clock.tsx
@@ -0,0 +1,22 @@
+import GLib from "gi://GLib?version=2.0";
+import { createExternal, createState } from "gnim";
+import BarComponent from "./BarComponent";
+
+export default function Clock() {
+  createState(GLib.DateTime.new_now_local);
+
+  const time = createExternal(GLib.DateTime.new_now_local(), (set) => {
+    const interval = setInterval(
+      () => set(() => GLib.DateTime.new_now_local()),
+      1000,
+    );
+
+    return () => clearInterval(interval);
+  });
+
+  return (
+    <BarComponent>
+      <label label={time((t) => t.format("%H:%M:%S") ?? "00:00:00")} />
+    </BarComponent>
+  );
+}
