033feebe57cfa43b1e3cde05e9640b6aa9198e85
NotLebedev <NotLebedev>
2025-08-27 16:29:11 +0300

    Add BarComponent and Clock
    Add base for all bar components and simple Clock component

--- widget/Bar.tsx
+++ widget/Bar.tsx
@@ -3,19 +3,12 @@ import { Astal, Gdk } from "ags/gtk4";
 import AutoHide from "./HideController";
 import { Props } from "./util";
 import { onCleanup } from "ags";
-
-function HideButton() {
-  return (
-    <button onClicked={(self) => console.log(self, "clicked")}>
-      <label label="Run" />
-    </button>
-  );
-}
+import Clock from "./Clock";
 
 export default function Bar({
   gdkmonitor,
 }: Props<{ gdkmonitor: Gdk.Monitor }>) {
-  const { TOP, LEFT, RIGHT } = Astal.WindowAnchor;
+  const { TOP, RIGHT } = Astal.WindowAnchor;
 
   let window!: Astal.Window;
   function resize() {
@@ -31,8 +24,9 @@ export default function Bar({
       class="Bar"
       gdkmonitor={gdkmonitor}
       exclusivity={Astal.Exclusivity.NORMAL}
-      anchor={TOP | LEFT | RIGHT}
+      anchor={TOP | RIGHT}
       application={app}
+      namespace="seashell"
       $={(self) => {
         window = self;
         onCleanup(() => self.destroy());
@@ -40,8 +34,7 @@ export default function Bar({
     >
       <AutoHide resizeHook={resize}>
         <box hexpand cssName="centerbox">
-          Text text text
-          <HideButton></HideButton>
+          <Clock />
         </box>
       </AutoHide>
     </window>
