c594f02a163ecf3ecd0c39f69da2da74bb98159f
NotLebedev <notlebedev@gmail.com>
2025-08-28 12:16:03 +0300

    Allow no children in ParentProps type

--- widget/util.ts
+++ widget/util.ts
@@ -1,8 +1,9 @@
 type PlainObject = { [key: string]: unknown };
 
 type Props<T extends PlainObject> = T;
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
 type ParentProps<T extends PlainObject = {}> = Props<T> & {
-  children: JSX.Element | Array<JSX.Element>;
+  children?: JSX.Element | Array<JSX.Element>;
 };
 
 export type { PlainObject, Props, ParentProps };
