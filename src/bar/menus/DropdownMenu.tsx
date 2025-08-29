afea2bd15109e852e1dec80cb9136cc0a8b733e2
NotLebedev <notlebedev@gmail.com>
2025-08-29 20:36:09 +0300

    Add power profile selector mokup

--- src/bar/menus/DropdownMenu.tsx
+++ src/bar/menus/DropdownMenu.tsx
@@ -1,9 +1,47 @@
 import { Gtk } from "ags/gtk4";
 
+function PowerProfileSelector() {
+  const saver = (
+    <togglebutton>
+      <image
+        iconName="power-profile-power-saver-symbolic"
+        iconSize={Gtk.IconSize.LARGE}
+      />
+    </togglebutton>
+  ) as Gtk.ToggleButton;
+  const balanced = (
+    <togglebutton group={saver}>
+      <image
+        iconName="power-profile-balanced-symbolic"
+        iconSize={Gtk.IconSize.LARGE}
+      />
+    </togglebutton>
+  ) as Gtk.ToggleButton;
+  const performance = (
+    <togglebutton group={balanced}>
+      <image
+        iconName="power-profile-performance-symbolic"
+        iconSize={Gtk.IconSize.LARGE}
+      />
+    </togglebutton>
+  ) as Gtk.ToggleButton;
+
+  saver.set_active(true);
+
+  return (
+    <box class="toggleGroup" hexpand homogeneous>
+      {saver}
+      {balanced}
+      {performance}
+    </box>
+  );
+}
+
 export default function DropdownMenu() {
   return (
-    <box>
+    <box orientation={Gtk.Orientation.VERTICAL}>
       <Gtk.Calendar />
+      <PowerProfileSelector />
     </box>
   );
 }
